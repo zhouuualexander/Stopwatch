0071722f86a243a43b61d07d971eebc7dd5e9417
zhouuualexander <zzhou3@ualberta.ca>
2021-07-19 22:36:22 -0600

    update copyright

--- src/Components/Copyright/Copyright.js
+++ src/Components/Copyright/Copyright.js
@@ -1,17 +1,39 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+let theme = createTheme({});
+theme = {
+  ...theme,
+  overrides: {
+    // Style sheet name ⚛️
+    MuiTypography: {
+      root: {
+        [theme.breakpoints.down("xs")]: {
+          paddingTop: "42vh"
 
+        },
+        paddingTop: "36vh"
+      },
+
+    },
+
+  },
+};
 const Copyright = () => {
+
   return (
-    <Typography variant="body2" color="textSecondary" align="center" style={{ marginTop: "40vh" }}>
-      {"Copyright © "}
-      <Link color="inherit" href="https://www.linkedin.cn/in/zijian-zhou/" underline="always" style={{ borderBottom: 'none' }}>
-        {"Zijian Zhou"}
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography >
+    <ThemeProvider theme={theme}>
+      <Typography variant="body2" color="textSecondary" align="center" >
+        {"Copyright © "}
+        <Link color="inherit" href="https://www.linkedin.cn/in/zijian-zhou/" underline="always" style={{ borderBottom: 'none' }}>
+          {"Zijian Zhou"}
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography >
+    </ThemeProvider>
+
   );
 
 };
